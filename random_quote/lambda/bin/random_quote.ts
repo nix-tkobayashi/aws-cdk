4cddb4b14099bdf334c4df351a3e59570316f061
nix_tkobayashi <github.com@kobayashi.in>
2020-05-27 21:44:26 +0900

    cdk study.

--- random_quote/lambda/bin/random_quote.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { RandomQuoteStack } from '../lib/random_quote-stack';
-
-const app = new cdk.App();
-new RandomQuoteStack(app, 'RandomQuoteStack');
